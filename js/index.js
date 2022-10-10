2171cd3e19cd194800cf5eb181046ce0574b8372
Carla Reis <carlareis.dev@gmail.com>
2022-10-10 00:35:19 -0400

    minor changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
+/////////////// nav bar collapse ////////////////
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  el_autohide = document.querySelector('.autohide');
+  
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    }); 
+  }
+});
+
+
+////////// copy email on click and alert ////////////
+
 function copy(that) {
   var inp = document.createElement("input");
   document.body.appendChild(inp);
@@ -7,8 +33,3 @@ function copy(that) {
   inp.remove();
   alert("Email Copied");
 }
-
-// function alertCopy() {
-// }
-//
-// document.getElementById("copy-email").addEventListener("mouseover", alertCopy);
