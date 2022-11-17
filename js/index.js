40e4df515b622be40024ea4ddeb0122a3c27e3f7
Carla Reis <carlareis.dev@gmail.com>
2022-11-17 19:45:59 -0400

    made img carousel work; need to adjust the image-box

--- js/index.js
+++ js/index.js
@@ -88,3 +88,49 @@ var data = {
     "clickToCopy": "Zum Kopieren klicken"
   }
 };
+
+// ***************** Gallery related **************** //
+
+// const zoomBtn = document.querySelectorAll('.gallery-img img');
+const allImages = document.querySelectorAll('.gallery-img');
+const imageView = document.querySelector('.image-view');
+const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
+const imageBox = document.querySelector('.image-box');
+
+let currentImageIdx = 0;
+
+imageView.addEventListener('click', function(){
+    this.style.display = "none";
+    imageBox.style.display = "none";
+})
+
+
+allImages.forEach(function(btn, index){
+    btn.addEventListener('click', function(){
+        imageView.style.display = "block";
+        imageBox.style.display = "block";
+        currentImageIdx = index + 1;
+        currentImageDisplay(currentImageIdx);
+    })
+})
+
+function currentImageDisplay(position){
+    imageBox.style.background = `url(css/img${currentImageIdx}.png) center/cover no-repeat`;
+}
+
+prevBtn.addEventListener('click', function(){
+    currentImageIdx--;
+    if(currentImageIdx === 0){
+        currentImageIdx = allImages.length;
+    }
+    currentImageDisplay(currentImageIdx);
+})
+
+nextBtn.addEventListener('click', function(){
+    currentImageIdx++;
+    if(currentImageIdx === 16){
+        currentImageIdx = 1;
+    }
+    currentImageDisplay(currentImageIdx);
+})
